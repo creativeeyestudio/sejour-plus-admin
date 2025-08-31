d84e9ae1daf5654a86d6de8b684806bb784a9582
creativeeyestudio <contact@creative-eye.fr>
2025-08-31 02:11:04 +0200

    Hotel Data API

---
+++ src/app/interfaces/hotel-data.ts
@@ -0,0 +1,13 @@
+export interface HotelData {
+    id: number,
+    hotelName: string,
+    hotelPhone: string,
+    hotelEmail: string,
+    hotelMap: string,
+    hotelWebsite: string,
+    hotelCheckIn: string | Date,
+    hotelCheckOut: string | Date,
+    hotelWifiName: string,
+    hotelWifiPassword: string,
+    hotelParking: string
+}
