14e1ea7c28915835ec66ecc301a7ef99a73cbe99
creativeeyestudio <contact@creative-eye.fr>
2025-09-01 19:29:40 +0200

    Create Service PopUp

--- src/app/pages/services/services.page.ts
+++ src/app/pages/services/services.page.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonLabel, IonItem, IonButton, IonButtons, IonModal, IonInput, IonTextarea } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.page.html',
   styleUrls: ['./services.page.scss'],
   standalone: true,
-  imports: [IonButton, IonItem, IonLabel, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonTextarea, IonInput, IonModal, IonButtons, IonButton, IonItem, IonLabel, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class ServicesPage implements OnInit {
+  @ViewChild(IonModal) modal!: IonModal;
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  closeModal() {
+    this.modal.dismiss(null, 'cancel');
   }
 
 }
