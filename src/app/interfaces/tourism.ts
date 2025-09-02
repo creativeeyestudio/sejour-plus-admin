17ddf0adeb2643d6c2ed3820f3960e55366dfe26
creativeeyestudio <contact@creative-eye.fr>
2025-09-02 21:24:19 +0200

    Tourism Interface

---
+++ src/app/interfaces/tourism.ts
@@ -0,0 +1,14 @@
+export interface Tourism {
+    id: number;
+    tourismName: string;
+    tourismContent: string;
+    tourismReserve: string;
+    tourismPhone: string;
+    tourismCategory: string;
+    tourismWebsite: string;
+}
+
+export interface TourismList {
+    member: Tourism[];
+    totalItems: number;
+}
