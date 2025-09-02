34fa4741955ee85ca89181641d33ebf720fdfdd9
creativeeyestudio <contact@creative-eye.fr>
2025-09-02 21:50:46 +0200

    Front-End Integration

--- src/app/pages/tourism/tourism.page.ts
+++ src/app/pages/tourism/tourism.page.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonList, IonItem, IonLabel, IonButton, IonButtons } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-tourism',
   templateUrl: './tourism.page.html',
   styleUrls: ['./tourism.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonButtons, IonButton, IonLabel, IonItem, IonList, IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
 })
 export class TourismPage implements OnInit {
 
