3ed82702cb29174a367f02f290d2472dc1aa8783
creativeeyestudio <contact@creative-eye.fr>
2025-09-02 09:15:24 +0200

    API Functions

--- src/app/services/api.ts
+++ src/app/services/api.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { HotelData, HotelPayload } from '../interfaces/hotel-data';
 import axios from 'axios';
+import { CategoriesList, Category } from '../interfaces/category';
 
 @Injectable({
   providedIn: 'root'
@@ -37,6 +38,51 @@ export class Api {
   async updateHotel(hotelPayload: Partial<HotelPayload>, id: number = 1) {
     try {
       const response = await axios.patch<HotelData>(`${this.hotelDataApi}/${id}`, hotelPayload)
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  // ------------------------------------------
+  // Catégories
+  // ------------------------------------------
+  private readonly categoriesApi: string = `${environment.apiUrl}/api/categories`;
+
+  async getCategories(): Promise<CategoriesList> {
+    try {
+      const response = await axios.get<CategoriesList>(`${this.categoriesApi}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Erreur lors de la récupération de l'API Catégories : ${error}`);
+      throw error;
+    }
+  }
+
+  async getCategory(id: number): Promise<Category> {
+    try {
+      const response = await axios.get<Category>(`${this.categoriesApi}/${id}`);
+      return response.data;
+    } catch (error) {
+      console.error(`Erreur lors de la récupération de l'API Catégorie avec ID ${id} : ${error}`);
+      throw error;
+    }
+  }
+
+  async updateCategory(categoryPayload: Partial<Category>, id: number) {
+    try {
+      const response = await axios.patch(`${this.categoriesApi}/${id}`, categoryPayload);
+      return response.data;
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+  }
+
+  async deleteCategory(id: number) {
+    try {
+      const response = await axios.delete(`${this.categoriesApi}/${id}`);
       return response.data;
     } catch (error) {
       console.error(error);
